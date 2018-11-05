4a839f76201a2c3fae7abd5c8b2edbebabdcf1c0
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-11-05 09:56:10 +0200

    deploy on server

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
-//    'admin_email' => 'elisoft.meb@gmail.com',
-    'admin_email' => 'dimaoag@gmail.com',
+    'admin_email' => 'elisoft.meb@gmail.com',
+//    'admin_email' => 'dimaoag@gmail.com',
     'shop_name' => 'ELISOFT',
     'pagination' => 6,
     'article_width' => 1920,
